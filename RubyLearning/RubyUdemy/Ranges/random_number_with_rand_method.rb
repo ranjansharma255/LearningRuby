488c4d6142f44dffa13264b500e8a0f91679b21c
ranjansharma255 <ranjansharma255@gmail.com>
2022-04-25 16:09:50 +0530

    added new file

---
+++ RubyLearning/RubyUdemy/Ranges/random_number_with_rand_method.rb
@@ -0,0 +1,12 @@
+puts rand.round(2)
+puts rand.round(3)
+
+puts rand(101) # till 100 this will generate the values
+
+
+puts rand * 100
+
+
+puts rand(56...60) # between the values random number is generated
+
+puts rand(1...100)
