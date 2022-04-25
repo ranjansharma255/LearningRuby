12b44167e052d6752a67df942df07506d556e985
ranjansharma255 <ranjansharma255@gmail.com>
2022-04-25 16:19:17 +0530

    arrays

---
+++ RubyLearning/RubyUdemy/arrays/intro_arrays.rb
@@ -0,0 +1,15 @@
+# p []
+# p [].class
+
+
+# number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# puts number
+# p number
+# print number
+# puts
+
+
+toys = ["Teddy bear", "Super Soaker", "board game"]
+
+things = [4, true, "Hello", 10.99]
+p things
