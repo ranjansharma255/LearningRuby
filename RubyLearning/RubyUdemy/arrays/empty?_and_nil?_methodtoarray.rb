a08ad494d24c4256a508d0bb61cd454c5bff161b
ranjansharma255 <ranjansharma255@gmail.com>
2022-04-26 10:35:45 +0530

    added new file

---
+++ RubyLearning/RubyUdemy/arrays/empty?_and_nil?_methodtoarray.rb
@@ -0,0 +1,21 @@
+puts [1, 2, 3].empty?
+
+puts [].empty?
+
+puts [].length == 0
+
+puts [false, false, false].empty? # false because the array is not empty
+
+puts [nil, nil].empty?
+
+puts [false, false, false].nil?
+
+puts 1.nil?
+
+letters = ("a".."j").to_a
+p letters
+
+character = letters[39]
+p character # here the character is nil because the 39th character is not present in the array.
+p character.nil? # when want to check it whether the nil value is there or not
+
